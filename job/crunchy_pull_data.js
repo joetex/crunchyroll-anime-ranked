5a7c6a64a85480e1812c323e0d5664e3350f6700
JoeTex <joelruiz2@gmail.com>
2025-11-21 14:42:08 -0600

    fixing security measurse by cloudflare

--- job/crunchy_pull_data.js
+++ job/crunchy_pull_data.js
@@ -15,7 +15,7 @@ let LANGUAGE = 'ja-JP';
 
 async function main() {
 
-    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'], });
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'], });
     const page = await browser.newPage();
 
     await page.setUserAgent(
